e54f78b972215e502b3ac0e9f500f2f4c881db41
Daniil Volfengaut <volfengaut@i-teco.ru>
2020-07-08 13:36:44 +0300

    New working Spring Boot Project

---
+++ src/main/java/daniil/tm2/Tm2Application.java
@@ -0,0 +1,13 @@
+package daniil.tm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tm2Application.class, args);
+	}
+
+}
---
+++ src/test/java/daniil/tm2/Tm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package daniil.tm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Tm2ApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
